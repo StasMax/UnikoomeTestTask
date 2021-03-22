7b7a060bc68b373659d97dba98c0b036e9cd03d0
Stanislav Maksimovich <stanislav.maksimovych@simbirsoft.com>
2021-03-22 20:05:34 +0300

    Правки маппера

--- src/main/java/org/example/userapi/common/mapper/UserMapper.java
+++ src/main/java/org/example/userapi/common/mapper/UserMapper.java
@@ -1,30 +1,46 @@
 package org.example.userapi.common.mapper;
 
+import org.example.userapi.common.enums.Sex;
 import org.example.userapi.domain.dto.UserDto;
 import org.example.userapi.domain.dto.UserShortDto;
 import org.example.userapi.domain.entity.User;
 import org.example.userapi.domain.vm.UserVM;
-import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import static org.example.userapi.common.constants.Constant.DATE_FORMATTER_PATTERN;
 
 @Mapper(componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        imports = {LocalDate.class})
-@DecoratedWith(UserMapperDecorator.class)
-public interface UserMapper {
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public abstract class UserMapper {
 
-    @Mapping(target = "sex", source = "entity.sex.sexName")
-    @Mapping(target = "birthDate", constant = "")
-    UserDto toDto(User entity);
+    public UserDto toDto(User entity) {
+        UserDto userDto = new UserDto();
+        userDto.setBirthDate(entity.getBirthDate().format(DateTimeFormatter.ofPattern(DATE_FORMATTER_PATTERN)));
+        userDto.setSex(entity.getSex().getSexName());
+        userDto.setEmail(entity.getEmail());
+        userDto.setFio(entity.getFio());
+        userDto.setUserName(entity.getUserName());
+        userDto.setId(entity.getId());
+        userDto.setPhotoUrl(entity.getPhotoUrl());
+        return userDto;
+    }
 
-    UserShortDto toShortDto(User entity);
+    public abstract UserShortDto toShortDto(User entity);
 
-    @Mapping(target = "sex", constant = "WOMAN")
-    @Mapping(target = "photoUrl", source = "photoUrl")
-    @Mapping(target = "birthDate", expression = "java(LocalDate.of(2019,12,30))")
-    User toEntity(UserVM vm, String photoUrl);
+   public User toEntity(UserVM vm, String photoUrl) {
+        User entity = new User();
+        entity.setFio(vm.getFio());
+        entity.setEmail(vm.getEmail());
+        entity.setUserName(vm.getUserName());
+        entity.setBirthDate(LocalDate.parse(vm.getBirthDate(), DateTimeFormatter.ofPattern(DATE_FORMATTER_PATTERN)));
+        entity.setSex(Arrays.stream(Sex.values()).filter(item -> item.getSexName().equals(vm.getSex())).findFirst().get());
+        entity.setPhotoUrl(photoUrl);
+        return entity;
+    }
+
 }
